00dcb23662f6994176f89c9097a474ecd5eab1f1
onderbedirhan <onderbedirhan00@gmail.com>
2024-06-04 23:00:15 +0300

    Change the demo code

--- demo.py
+++ demo.py
@@ -1,48 +1,42 @@
+import sys
 import numpy as np
 import cv2
-import os
+import vitis_ai_library
+import xir
 import time
-import argparse
-import tensorflow as tf
+import os
 
-# For using GPU or CPU
-parser = argparse.ArgumentParser(description='Set GPU usage for TensorFlow')
-parser.add_argument('--use_gpu', action='store_true', help='Use GPU if available')
-args = parser.parse_args()
+# Check if the model name argument is provided
+if len(sys.argv) != 2:
+    print("Usage: python script.py <model_name>")
+    sys.exit(1)
 
-if args.use_gpu:
-    gpus = tf.config.list_physical_devices('GPU')
-    if gpus:
-        try:
-            for gpu in gpus:
-                tf.config.experimental.set_memory_growth(gpu, True)
-            print("GPU is enabled and configured.")
-        except RuntimeError as e:
-            print(e)
-    else:
-        print("No GPU devices found.")
-else:
-    print("GPU usage is disabled. Running on CPU.")
+DPU_CONFIG = sys.argv[1]
 
-# Constants
-MODEL_PATH = "flower_model.h5"
+# File path
+MODEL_PATH = f"./xmodel_outputs/{DPU_CONFIG}/{DPU_CONFIG}.xmodel"
+
+# Initialize camera
 CAMERA_INDEX = 0  # Default camera index
-
-flower_labels = ["daisy", "dandelion", "rose", "sunflower", "tulip"]
-
-# Load the model
-model = tf.keras.models.load_model(MODEL_PATH)
-print("Model loaded successfully")
-
-# Open the camera
 cap = cv2.VideoCapture(CAMERA_INDEX)
 if not cap.isOpened():
     print("Error: Could not open camera.")
-    exit()
+    sys.exit(1)
 
 all_process_start_time = time.time()
+
+g = xir.Graph.deserialize(MODEL_PATH)
+runner = vitis_ai_library.GraphRunner.create_graph_runner(g)
+
+# Input buffer
+inputDim = tuple(runner.get_inputs()[0].get_tensor().dims)
+inputData = [np.empty(inputDim, dtype=np.int8)]
+
+# Variables to calculate FPS
+frame_count = 0
+fps = 0
+start_time = time.time()
 total_inference_duration_ms = 0
-num_frames = 0
 
 while True:
     ret, frame = cap.read()
@@ -50,35 +44,60 @@ while True:
         print("Error: Could not read frame.")
         break
 
-    image = cv2.resize(frame, (224, 224))
-    image = np.expand_dims(image, axis=0)
-    image = image / 255.0  # normalization
+    # Normalization
+    image = frame / 255.0
+
+    # Quantization
+    fix_point = runner.get_input_tensors()[0].get_attr("fix_point")
+    scale = 2 ** fix_point
+    image = (image * scale).round()
+    image = image.astype(np.int8)
+
+    # Set input data
+    inputData[0][0] = image
 
     # Start timing for inference
     inference_start_time = time.time()
 
-    # Predict
-    predictions = model.predict(image, verbose=0)
-    idx = np.argmax(predictions[0])
+    # Output buffer
+    outputData = runner.get_outputs()
 
-    # Inference duration
+    # Prediction
+    job_id = runner.execute_async(inputData, outputData)
+    runner.wait(job_id)
+
     inference_end_time = time.time()
+
+    # Inference duration for this frame
     inference_duration_ms = (inference_end_time - inference_start_time) * 1000
     total_inference_duration_ms += inference_duration_ms
-    num_frames += 1
 
-    # Display the prediction on the frame
-    label = f"{flower_labels[idx]} ({predictions[0][idx]:.5f})"
-    cv2.putText(frame, label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    resultList = np.asarray(outputData[0])[0]
+    resultIdx = resultList.argmax()
+    resultVal = resultList[0][0][resultIdx]
+
+    # Calculate FPS
+    frame_count += 1
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    if elapsed_time > 1:
+        fps = frame_count / elapsed_time
+        frame_count = 0
+        start_time = time.time()
+
+    # Display the FPS on the frame
+    fps_text = f"FPS: {fps:.2f}"
+    cv2.putText(frame, fps_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
     # Show the frame
-    cv2.imshow('Real-time Inference', frame)
+    cv2.imshow('Real-time Video Stream', frame)
 
     # Exit if 'q' is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # Summarize the process
+num_frames = frame_count
 average_inference_time_ms = total_inference_duration_ms / num_frames
 print(f"Average inference duration: {average_inference_time_ms:.5f} ms")
 print(f"Total inference duration: {total_inference_duration_ms:.5f} ms")
@@ -90,3 +109,5 @@ print(f"All process duration: {all_process_duration_ms:.5f} ms")
 # Release the camera and close all windows
 cap.release()
 cv2.destroyAllWindows()
+
+del runner
